2f729af55ac81cd871c949340694bc8a7a265663
nguyentrungnam <namntse170246@fpt.edu.vn>
2024-09-11 14:23:05 +0700

    Upload CRUD branch

---
+++ CRUD/CRUD/Controllers/HangHoaController.cs
@@ -0,0 +1,37 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(HangHoa hangHoaVM)
+        {
+            var hangHoa = new HangHoaVM
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia,
+            };
+
+            return Ok(new
+            {
+                success = true,
+                data = hangHoa,
+            });
+        }
+    }
+}
---
+++ CRUD/CRUD/Models/HangHoa.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD.Models
+{
+    public class HangHoa
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+
+    }
+
+    public class HangHoaVM : HangHoa
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
